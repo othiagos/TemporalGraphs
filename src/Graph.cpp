d37cce8ab1b70d90540545227474e8477f73c7c2
othiagos <thg.santos@outlook.com>
2023-10-01 10:09:20 -0300

    optimize smart pointers

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -42,15 +42,15 @@ const std::vector<std::shared_ptr<Village>> &Graph::get_villages() const {
 
 void Graph::add_village() {
     uint32_t index = get_villages().size();
-    std::shared_ptr<Village> village(new Village(index));
+    auto village = std::make_shared<Village>(index);
     this->m_villages.push_back(village);
 }
 
 void Graph::add_connection(uint32_t first_index_village, uint32_t second_index_village,
                            uint32_t age_conn, uint32_t crossing_time, uint32_t construction_cost) {
 
-    std::shared_ptr<Connection> conn(new Connection(first_index_village, second_index_village,
-                                                    age_conn, crossing_time, construction_cost));
+    auto conn = std::make_shared<Connection>(first_index_village, second_index_village,
+                                             age_conn, crossing_time, construction_cost);
 
     this->m_villages.at(first_index_village)->add_connection_village(conn);
     this->m_villages.at(second_index_village)->add_connection_village(conn);
